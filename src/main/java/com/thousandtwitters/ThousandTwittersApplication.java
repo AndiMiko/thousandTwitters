a802e2bc07a1e670782e24d9d89139d0363f6306
Andreas Mikolajewski <andi_miko@yahoo.de>
2017-01-24 02:54:11 +0100

    Setup project

---
+++ src/main/java/com/thousandtwitters/ThousandTwittersApplication.java
@@ -0,0 +1,14 @@
+package com.thousandtwitters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThousandTwittersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThousandTwittersApplication.class, args);
+	}
+
+
+}
