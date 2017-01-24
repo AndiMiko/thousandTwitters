7b2abbf8e8a57f8714a9e4aaba573288811df167
Andreas Mikolajewski <andi_miko@yahoo.de>
2017-01-24 05:51:59 +0100

    Add basic database initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO user (Username, Email)
+VALUES ('Alice', 'alice@thousandtwitters.com');
+
+INSERT INTO user (Username, Email)
+VALUES ('Bob', 'bob@gmail.com');
+
+INSERT INTO user (Username, Email)
+VALUES ('John', 'john@johnsdomain.com');
+
+INSERT INTO follows (Follower, Followed)
+VALUES ('1', '2');
+
+INSERT INTO follows (Follower, Followed)
+VALUES ('1', '3');
+
+INSERT INTO follows (Follower, Followed)
+VALUES ('2', '1');
+
+INSERT INTO tweet (User, Text)
+VALUES ('1', 'Finally a fresh Twitter, always disliked the old rusty one');
+
+INSERT INTO tweet (User, Text)
+VALUES ('1', 'I have never understood the timeline of twitter anyways');
+
+INSERT INTO tweet (User, Text)
+VALUES ('1', 'SO HAPPY :). Those features here are amazing!');
+
+INSERT INTO tweet (User, Text)
+VALUES ('1', 'Limiting tweets here to 139 characters was just brilliant, always disliked 140 @ old Twitter!');
+
+INSERT INTO tweet (User, Text)
+VALUES ('3', 'Please dont read my tweets, i want to be private');
